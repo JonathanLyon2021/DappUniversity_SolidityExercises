39088175ab9d7de6a2d51d8c8ff77708ab1ef111
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-02 12:55:50 -0400

    Update Counter.sol
    Deleted function/method getCount().

--- Counter.sol
+++ Counter.sol
@@ -4,10 +4,6 @@ pragma solidity ^0.8.0;
 contract Counter {
     uint256 public count = 0;
     
-    function getCount() public view returns(uint) {
-        return count;
-    }
-    
      function incrementCount() public {
         //count = count + 1;
         count++;
