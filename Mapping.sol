26596a3747f2e8e00856984b902662410157b58e
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-04 19:43:21 -0400

    Update Mapping.sol
    Added public function addMyBook() which takes 3 parameters also: _id(uint), _title(string), _author(string).

--- Mapping.sol
+++ Mapping.sol
@@ -6,6 +6,7 @@ contract MyContract {
     //mapping(key => value)
     mapping(uint => string) names;
     mapping(uint => Book) public books;
+    mapping(address => mapping(uint => Book)) public myBooks;
 
     struct Book {
         string title;
@@ -20,6 +21,10 @@ contract MyContract {
 
     function addBook(uint _id, string memory _title, string memory _author) public {
         books[_id] = Book(_title, _author);
+    }
+
+     function addMyBook(uint _id, string memory _title, string memory _author) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
     }
 
 }
