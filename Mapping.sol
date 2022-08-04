16f10296b5161ecbca4be176b351f20ef020ed8b
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-04 19:30:32 -0400

    Update Mapping.sol
    Added struct Book.

--- Mapping.sol
+++ Mapping.sol
@@ -6,6 +6,11 @@ contract MyContract {
     //mapping(key => value)
     mapping(uint => string) names;
 
+    struct Book {
+        string title;
+        string author;
+    }
+
     constructor() public {
         names[1] = "Adam";
         names[2] = "Bruce";
