dde03d15acdc6f09d1b29121d4bddf213ba55cf4
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-06 21:58:42 -0400

    Update Inheritance.sol

--- Inheritance.sol
+++ Inheritance.sol
@@ -6,5 +6,12 @@ pragma solidity ^0.8.0;
 contract Ownable {
     address owner;
     
-    
+     constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
 }
