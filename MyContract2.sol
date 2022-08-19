539b91cdeee08cd3293ec0285ee67d5d068297ac
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-18 23:29:28 -0400

    Update MyContract2.sol

--- MyContract2.sol
+++ MyContract2.sol
@@ -10,5 +10,9 @@ contract MyContract {
     function get() public view returns(string) {
         return value;
     }
+    
+    function set(string _value) public {
+        value = _value;
+    }
 }
 
